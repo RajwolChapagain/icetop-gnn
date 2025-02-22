077d894ba5d68630705b0c5bf7437819a2313034
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-02-22 16:21:20 -0600

    Update data paths

--- data_converter.py
+++ data_converter.py
@@ -4,13 +4,12 @@ from graphnet.data.dataconverter import DataConverter
 from graphnet.data.readers import I3Reader
 from graphnet.data.writers import SQLiteWriter
 
-# Your settings
-dir_with_files = 'test_data'
-outdir = 'test_data_out'
+dir_with_files = '~/data/'
+outdir = 'data_out/'
 num_workers = 1
 
 # Instantiate DataConverter - exports data from I3 to SQLite
-converter = DataConverter(file_reader = I3Reader('data/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
+converter = DataConverter(file_reader = I3Reader(dir_with_files + 'gcd/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
                           save_method = SQLiteWriter(),
                           extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(name = "features")],
                           outdir=outdir,
@@ -18,5 +17,3 @@ converter = DataConverter(file_reader = I3Reader('data/GeoCalibDetectorStatus_20
                         )
 # Run Converter
 converter(input_dir = dir_with_files)
-# Merge files (Optional)
-converter.merge_files()
