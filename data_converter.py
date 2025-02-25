a5803e34e92eec8e823f5d6cb6d39092d227e663
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-02-25 13:57:43 -0600

    Merge db and remove num_workers

--- data_converter.py
+++ data_converter.py
@@ -6,14 +6,13 @@ from graphnet.data.writers import SQLiteWriter
 
 dir_with_files = '/home/rchapagain/data/'
 outdir = 'data_out/'
-num_workers = 1
 
 # Instantiate DataConverter - exports data from I3 to SQLite
 converter = DataConverter(file_reader = I3Reader(dir_with_files + 'gcd/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
                           save_method = SQLiteWriter(),
                           extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(name = "features")],
                           outdir=outdir,
-                          num_workers=num_workers,
                         )
 # Run Converter
 converter(input_dir = dir_with_files)
+converter.merge_files()
