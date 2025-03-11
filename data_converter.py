f196b170629a4e38336c6e7bf6d408c4eb258e25
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-03-11 10:34:46 -0500

    Use new ITFeatureExtractor

--- data_converter.py
+++ data_converter.py
@@ -1,5 +1,5 @@
 from graphnet.data.extractors.icecube.ittruthextractor import ITTruthExtractor
-from graphnet.data.extractors.icecube.itfeatureextractor import ITFeatureExtractor
+from graphnet.data.extractors.icecube.i3featureextractor import ITFeatureExtractor
 from graphnet.data.dataconverter import DataConverter
 from graphnet.data.readers import I3Reader
 from graphnet.data.writers import SQLiteWriter
@@ -10,7 +10,7 @@ outdir = 'data_out/'
 # Instantiate DataConverter - exports data from I3 to SQLite
 converter = DataConverter(file_reader = I3Reader(dir_with_files + 'gcd/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
                           save_method = SQLiteWriter(),
-                          extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(name = "features")],
+                          extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(pulsemap = "OfflineIceTopHLCTankPulses")],
                           outdir=outdir,
                         )
 # Run Converter
