bfec9774772d990287bee59ba3bc2ca6db0b10c6
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-03-25 06:26:36 -0500

    Select 100 random events for tranining

--- dataset_definition.py
+++ dataset_definition.py
@@ -13,13 +13,13 @@ dataset = SQLiteDataset(
     path="data_out/merged/merged.db",
     graph_definition=graph_definition,
     pulsemaps="OfflineIceTopHLCTankPulses",
-    features=["dom_x", "dom_y", "dom_time", "charge"], 
+    features=["charge", "dom_time", "dom_x", "dom_y"], 
     truth_table="truth_table",
     truth=["energy"],
 )
 
 dataset.config.selection = {
-        "train": "event_no % 2 == 0",
+        "train": "100 random events ~ event_no % 2 == 0",
         "test": "event_no % 2 == 1",
 }
 
