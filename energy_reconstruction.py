74025e75dd20a391c5510889ee1f45aa2eefd55b
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-09-08 15:58:47 -0500

    Utilize gpu for training and prediction and save results

--- energy_reconstruction.py
+++ energy_reconstruction.py
@@ -5,6 +5,7 @@ from graphnet.constants import CONFIG_DIR  # Local path to graphnet/configs
 from graphnet.data.dataloader import DataLoader
 from graphnet.models import Model
 from graphnet.utilities.config import DatasetConfig, ModelConfig
+from graphnet.data.dataset import Dataset
 
 # Configuration
 dataset_config_path = f"test.yml"
@@ -19,21 +20,27 @@ dataset_config = DatasetConfig.load(dataset_config_path)
 dataloaders = DataLoader.from_dataset_config(
     dataset_config,
     batch_size=16,
-    num_workers=63,
+    num_workers=24,
 )
 
 # Train model
 model.fit(
     dataloaders["train"],
-    dataloaders["test"],# Needs to be validation instead of test since we're also predicting test
-    max_epochs=10,
+    max_epochs=50,
+    gpus=[0],
 )
 
 # Predict on test set and return as pandas.DataFrame
 results = model.predict_as_dataframe(
     dataloaders["test"],
     additional_attributes=model.target_labels + ["event_no"],
+    gpus=[0],
 )
 
 # Save predictions and model to file
 print(results)
+outdir = "predictions"
+os.makedirs(outdir, exist_ok=True)
+results.to_csv(f"{outdir}/results.csv")
+model.save_state_dict(f"{outdir}/state_dict.pth")
+model.save(f"{outdir}/model.pth")
