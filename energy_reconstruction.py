b08f7db385aaf667053d85e3a176fe5898126085
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-09-22 14:43:58 +0000

    Increase batch size, add early stopping and checkpointing callbacks, increase epoch count

--- energy_reconstruction.py
+++ energy_reconstruction.py
@@ -1,15 +1,16 @@
 # Import(s)
 import os
 
-from graphnet.constants import CONFIG_DIR  # Local path to graphnet/configs
 from graphnet.data.dataloader import DataLoader
 from graphnet.models import Model
 from graphnet.utilities.config import DatasetConfig, ModelConfig
 from graphnet.data.dataset import Dataset
+from graphnet.training.callbacks import ProgressBar, EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 # Configuration
-dataset_config_path = f"test.yml"
-model_config_path = f"{CONFIG_DIR}/models/icetop_energy_reconstruction_model.yml"
+dataset_config_path = f"configs/dataset_config.yml"
+model_config_path = f"configs/model_config.yml"
 
 # Build model
 model_config = ModelConfig.load(model_config_path)
@@ -19,15 +20,24 @@ model = Model.from_config(model_config, trust=True)
 dataset_config = DatasetConfig.load(dataset_config_path)
 dataloaders = DataLoader.from_dataset_config(
     dataset_config,
-    batch_size=16,
+    batch_size=128,
     num_workers=24,
 )
+
+# Callbacks
+callbacks = [
+    ProgressBar(),
+    EarlyStopping(monitor="val_loss", patience=15, mode="min"),  # stop if no progress
+    ModelCheckpoint(dirpath="checkpoints", save_top_k=1, monitor="val_loss", mode="min"),
+]
 
 # Train model
 model.fit(
     dataloaders["train"],
-    max_epochs=50,
+    dataloaders["validation"],
+    max_epochs=200,
     gpus=[0],
+    callbacks=callbacks,
 )
 
 # Predict on test set and return as pandas.DataFrame
@@ -39,7 +49,7 @@ results = model.predict_as_dataframe(
 
 # Save predictions and model to file
 print(results)
-outdir = "predictions"
+outdir = "outputs"
 os.makedirs(outdir, exist_ok=True)
 results.to_csv(f"{outdir}/results.csv")
 model.save_state_dict(f"{outdir}/state_dict.pth")
