ceebd8ac99eea804261368ffcb2718c72d7033ca
Rajwol Chapagain <rchapagain@icecube.wisc.edu>
2025-11-30 04:56:36 +0000

    Require arguments

--- data_converter.py
+++ data_converter.py
@@ -3,16 +3,36 @@ from graphnet.data.extractors.icecube.i3featureextractor import ITFeatureExtract
 from graphnet.data.dataconverter import DataConverter
 from graphnet.data.readers import I3Reader
 from graphnet.data.writers import SQLiteWriter
+import argparse
+import os
+import multiprocessing
 
-dir_with_files = '/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012/oldstructure/12360'
-outdir = '/home/rchapagain/icetop-gnn/data_out/'
+parser = argparse.ArgumentParser(description="")
+
+parser.add_argument(
+    "--indir", "-i",
+    type=str,
+    default="/data/ana/CosmicRay/IceTop_level3/sim/IC86.2012/oldstructure/12360",
+    help="Directory containing the i3 files (default: %(default)s)"
+)
+
+parser.add_argument(
+    "--outdir", "-o",
+    type=str,
+    required=True,
+    help="Empty directory where output .db files are saved"
+)
+
+args = parser.parse_args()
+os.makedirs(args.outdir)
 
 # Instantiate DataConverter - exports data from I3 to SQLite
-converter = DataConverter(file_reader = I3Reader('/home/rchapagain/data/gcd/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
+converter = DataConverter(file_reader = I3Reader('/data/user/rchapagain/GeoCalibDetectorStatus_2012.56063_V1_OctSnow_scint.i3'),
                           save_method = SQLiteWriter(),
-                          extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(pulsemap = "OfflineIceTopHLCTankPulses")],
-                          outdir=outdir,
+                          extractors=[ITTruthExtractor(name = "truth_table"), ITFeatureExtractor(pulsemap = "OfflineIceTopSLCTankPulses"), ITFeatureExtractor(pulsemap = "OfflineIceTopHLCTankPulses")],
+                          outdir=args.outdir,
+                          num_workers = multiprocessing.cpu_count(),
                         )
 # Run Converter
-converter(input_dir = dir_with_files)
+converter(input_dir = args.indir)
 converter.merge_files()
